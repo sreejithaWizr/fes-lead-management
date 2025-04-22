3a20abe9252aeed451a6767583208b534cb5e322
Aravinth Raj J <aravinthraj@thoughtminds.io>
2025-04-22 23:36:13 +0530

    feat(layout): Added responive layout for login

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,17 @@ module.exports = {
 	prefix: "",
 	theme: {
 	  container: {
+		screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1400px',
+		  },
 		center: true,
 		padding: "2rem",
 		screens: {
-		  "2xl": "1400px",
+			'2xl': '1400px',
 		},
 	  },
 	  extend: {
