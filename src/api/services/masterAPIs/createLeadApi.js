f3574e2c00ca06b778d243438938776c94eff602
AlbenJoseph <alben.joseph@wizr.ai>
2025-04-23 14:24:44 +0530

    API endpoints added frp create lead

---
+++ src/api/services/masterAPIs/createLeadApi.js
@@ -0,0 +1,34 @@
+import apiClient from "../../config/axios";
+
+// Endpoints of dropdown APIs - Create Lead 
+export const getAreaOfStudy = () => apiClient.get('api/AreaOfStudy');
+
+export const getBranch = () => apiClient.get('api/Branch');
+
+export const getCategory = () => apiClient.get('api/Category');
+
+export const getCity = () => apiClient.get('api/City');
+
+export const getCountry = () => apiClient.get('api/Country');
+
+export const getFESUser = () => apiClient.get('api/FESUser');
+
+export const getOrganization = () => apiClient.get('api/Organization');
+
+export const getOrganizationType = () => apiClient.get('api/OrganizationType');
+
+export const getPriority = () => apiClient.get('api/Priority');
+
+export const getQualification = () => apiClient.get('api/Qualification');
+
+export const getRegion = () => apiClient.get('api/Region');
+
+export const getSource1 = () => apiClient.get('api/Source1');
+
+export const getStatus = () => apiClient.get('api/Status');
+
+export const getSubCategory = () => apiClient.get('api/SubCategory');
+
+export const getTestName = () => apiClient.get('api/TestName');
+
+export const getVertical = () => apiClient.get('api/Vertical');
