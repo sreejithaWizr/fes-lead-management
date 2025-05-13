7133f111b138ff332b511c453ddeb55faa3030ca
sreejithaWizr <sreejitha.mc@wizr.ai>
2025-05-13 12:05:53 +0530

    organization create is in progress

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,14 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import leadsReducer from './leadsSlice';
-import lorganisationsReducer from './organisationSlice'
+import organisationsReducer from './organisationSlice'
 import userReducer from './userSlice';
 import rolesReducer from './roleSlice'
 
 export const store = configureStore({
   reducer: {
     leads: leadsReducer,
-    organisations: lorganisationsReducer,
+    organisations: organisationsReducer,
     users: userReducer,
     roles: rolesReducer,
   },
