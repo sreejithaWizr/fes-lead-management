eb9d7a54108cfea0d41a89a6a2b4c16c69c173c9
avnashv <avinash.vijayan@wizr.ai>
2025-05-13 15:47:58 +0530

    added drag option in user-listing table

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,13 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const columns = [
-    { column: "userName", label: "Name", id: "userName", showSort: true, isDrag: false, isFilter: true },
-    { column: "userRole", label: "Role", id: "userRole", showSort: true, isDrag: false, isFilter: true },
-    { column: "orgName", label: "Org Name", id: "orgName", showSort: true, isDrag: false, isFilter: true },
-    { column: "orgType", label: "Org Type", id: "orgType", showSort: true, isDrag: false, isFilter: true },
-    { column: "userStatus", label: "Status", id: "userStatus", showSort: true, isDrag: false, isFilter: true },
-    { column: "userBranch", label: "Branch", id: "userBranch", showSort: true, isDrag: false, isFilter: true },
-    { column: "action", label: "Actions", id: "action", showSort: false, isDrag: false, isFilter: false },
+    { column: "userName", label: "Name", id: "userName", showSort: true, isDrag: true, isFilter: true },
+    { column: "userRole", label: "Role", id: "userRole", showSort: true, isDrag: true, isFilter: true },
+    { column: "orgName", label: "Org Name", id: "orgName", showSort: true, isDrag: true, isFilter: true },
+    { column: "orgType", label: "Org Type", id: "orgType", showSort: true, isDrag: true, isFilter: true },
+    { column: "userStatus", label: "Status", id: "userStatus", showSort: true, isDrag: true, isFilter: true },
+    { column: "userBranch", label: "Branch", id: "userBranch", showSort: true, isDrag: true, isFilter: true },
+    { column: "action", label: "Actions", id: "action", showSort: false, isDrag: true, isFilter: false },
 ];
 
 const userSlice = createSlice({
